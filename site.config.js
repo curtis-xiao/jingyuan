ad55cb87c34e190dc04f997a0a8e49d5187f3103
curtis-xiao <96342525+curtis-xiao@users.noreply.github.com>
2021-12-18 22:17:17 +0800

    Update site.config.js
    update configs

--- site.config.js
+++ site.config.js
@@ -13,7 +13,7 @@ module.exports = {
 
   // open graph metadata (optional)
   description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
+  socialImageTitle: 'Curtis Xiao',
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
